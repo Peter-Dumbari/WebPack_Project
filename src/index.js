9354d539daf3b1b7bfd83d0eee8729a401449985
TAMS-TECHNOLOGY <dummytwiz@gmail.com>
2023-05-10 15:30:31 +0100

    updates according to the requested changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 import './style.css';
 
-function component() {}
+const activities = [
+  {
+    description: 'Wash the dishes',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Complete the To Do list project',
+    completed: true,
+    index: 2,
+  },
+];
 
-document.body.appendChild(component());
+function renderTodoList() {
+  const todoList = document.querySelector('.full-list');
+  activities.sort((a, b) => a.index - b.index);
+  todoList.innerHTML = activities
+    .map(
+      (task) => `<li class="todo-item ${task.completed ? 'completed' : ''}">
+          <input type="checkbox" ${task.completed ? 'checked' : ''}>
+          <span>${task.description}</span>
+          <hr class="todo-hr">
+        </li>`,
+    )
+    .join('');
+}
+
+renderTodoList();
