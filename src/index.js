f45c617ff82dd92575c299beba735db7f0fb0fb4
TAMS-TECHNOLOGY <dummytwiz@gmail.com>
2023-05-10 15:40:39 +0100

    final update according to the requested changes

--- src/index.js
+++ src/index.js
@@ -14,9 +14,22 @@ const activities = [
 ];
 
 function renderTodoList() {
-  const todoList = document.querySelector('.full-list');
+  const todoList = document.querySelector('.list_container');
   activities.sort((a, b) => a.index - b.index);
-  todoList.innerHTML = activities
+  todoList.innerHTML = ` <div class="main-list">
+  <div class="item-container">
+      <div>
+          <div class="today-content white-height">
+              <span class="heading">Today's To Do list</span>
+              <i class="fas fa-sync"></i>
+          </div>
+          <form class="add-new white-height">
+              <input class="no-outline" maxlength="128" id="new-item" type="text"
+                  placeholder="Add to your list ..." />
+              <i class="fas fa-level-down-alt"></i>
+          </form>
+          <div class="full-list">
+${activities
     .map(
       (task) => `<li class="todo-item ${task.completed ? 'completed' : ''}">
           <input type="checkbox" ${task.completed ? 'checked' : ''}>
@@ -24,7 +37,14 @@ function renderTodoList() {
           <hr class="todo-hr">
         </li>`,
     )
-    .join('');
+    .join('')}
+          </div>
+      </div>
+  </div>
+  <div class="clear">
+      <a href="#" title="clear all" id="clear-text">Clear all completed</a>
+  </div>
+</div>`;
 }
 
 renderTodoList();
